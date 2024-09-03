e86912f2ae2f6bba3b4667b16e4e86462422151d
Gabby-Tech1 <gkaquainoo@gmail.com>
2024-09-03 16:20:12 +0000

    Completed day 1

---
+++ Day 1/Day 1 project/main.py
@@ -0,0 +1,8 @@
+#1. Create a greeting for you program
+print("Hello, Welcome to my brand name generator")
+#2. Ask the user for the city that they grew up in
+city = input("Which city did you grew up in?\n")
+#3. Ask the user for the name of a pet
+pet = input("What is the name of your pet?\n")
+#4. Combine the name of their city and pet and show them their brand name
+print("Your brand name will be " + city + " " + pet)
