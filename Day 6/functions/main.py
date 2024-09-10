2bdf082e1960a68a06fbfddf71ebf0b31eb30b7d
Gabby-Tech1 <gkaquainoo@gmail.com>
2024-09-10 11:50:25 +0000

    Day 6 done

---
+++ Day 6/functions/main.py
@@ -0,0 +1,5 @@
+# function are declared using the "def" keyword
+
+# Example
+def function_name():
+    pass
---
+++ Day 6/while loop/main.py
@@ -0,0 +1,6 @@
+# While loops is used to iterate through a sequence until a condition is false
+
+# Example
+while condition:
+    # do this
+    pass
